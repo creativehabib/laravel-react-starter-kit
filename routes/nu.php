e5554e915b8108d4654119ec9695d88334c0f2b1
creativehabib <shamolrahaman@gmail.com>
2025-04-22 18:20:23 +0600

    update

--- routes/nu.php
+++ routes/nu.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     // Designation Routes
     Route::resource('designations', DesignationController::class);
-    Route::put('/designations/{id}/toggle-status', [DesignationController::class, 'toggleStatus'])->name('designations.toggle-status');
+    Route::match(['put', 'post'], '/designations/{id}/toggle-status', [DepartmentController::class, 'toggleStatus'])
+    ->name('designations.toggle-status');
 
     // Department Routes
     Route::resource('departments', DepartmentController::class)->names('departments');
-    Route::put('/departments/{id}/toggle-status', [DepartmentController::class, 'toggleStatus'])->name('departments.toggle-status');
-
+    Route::match(['put', 'post'], '/departments/{id}/toggle-status', [DepartmentController::class, 'toggleStatus'])
+    ->name('departments.toggle-status');
     // Post Routes
     Route::resource('posts', PostController::class)->names('posts');
     Route::put('/posts/{id}/toggle-status', [PostController::class, 'toggleStatus'])->name('posts.toggle-status');
